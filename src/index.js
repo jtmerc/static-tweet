78148a557d6afd148fcf6bbdac2981b44959dc05
jadentm <jadentm@hotmail.com>
2021-06-20 17:44:27 -0500

    tweet

--- src/index.js
+++ src/index.js
@@ -4,14 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Tweet() {
+  return (
+    <div className="tweet">
+      <Avatar />
+      <div className="content">
+        <NameWithHandle /> <Time />
+        <Message />
+        <div className="button">
+          <CommentButton />
+          <RetweetButton />
+          <LikeButton />
+          <ShareButton />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src='https://gravatar.com/avatar/nothing'
+      alt='avatar'
+      className='avatar'
+    />
+  );
+}
+
+function Message() {
+  return (
+    <div className="message">This message is less than 140 characters!</div>
+  );
+}
+
+function NameWithHandle() {
+  return (
+    <span className="name-with-handle">
+      <span className="name">Your Name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+
+const Time = () => <span className="time">3h ago</span>;
+
+const CommentButton = () => <i className="far fa-comment" />;
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+const ShareButton = () => <i className="fas fa-external-link-alt" />;
+
+
+
+ReactDOM.render(<Tweet />, document.querySelector('#root'));
+
 reportWebVitals();
